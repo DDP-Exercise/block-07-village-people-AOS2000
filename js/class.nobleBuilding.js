f270446e67712ea5b0318aefd47cf5f408cbe680
AOS2000 <s2510892024@fhooe.at>
2026-06-16 07:01:02 +0200

    main

--- js/class.nobleBuilding.js
+++ js/class.nobleBuilding.js
@@ -1,6 +1,17 @@
 "use strict";
+import Building from "./class.building.js";
+import NobleCitizen from "./class.nobleCitizen.js";
 
 /**
  * Create a NobleBuilding class. It's basically the same as a regular building
  * with one exception: Only nobles allowed.
  */
+export default class NobleBuilding extends Building {
+
+    addResident(citizen) {
+        if (!(citizen instanceof NobleCitizen))
+            return false;
+
+        return super.addResident(citizen);
+    }
+}
